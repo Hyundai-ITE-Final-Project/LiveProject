e388ede201dd5ed581711e82f2a152dfe35c4f29
Shingiwon <65998782+Shingiwon@users.noreply.github.com>
2023-01-12 10:46:38 +0900

    feat create member service #19

--- LiveCommerce/src/main/java/com/livecommerce/project/service/MemberServiceImpl.java
+++ LiveCommerce/src/main/java/com/livecommerce/project/service/MemberServiceImpl.java
@@ -1,11 +1,81 @@
 package com.livecommerce.project.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.livecommerce.project.mapper.MemberMapper;
+import com.livecommerce.project.vo.MemberVO;
+
+/**
+ * @author 신기원
+ * @since 2023.01.12
+ * @version 1.0
+ * 
+ * <pre>
+ * 수정일                    수정자                   수정내용
+ * ----------  --------    ---------------------------
+ * 2023.01.12    신기원                	 최초 생성
+
+ * </pre>
+ */
 
 @Service
 @Transactional(readOnly=true)
-public class MemberServiceImpl {
+public class MemberServiceImpl implements MemberService{
+	
+	@Autowired
+	private MemberMapper memberMapper;
+	
+	@Autowired
+	private PasswordEncoder encoder;
+	
+	/**
+     * 메서드 설명 : 컨트롤러로 전달 받은 패스워드 암호화
+     *
+     * @param   memberVO
+     * @return  String
+     */
+	
+	private String password(String mpassword) {
+		return encoder.encode(mpassword);
+		
+	}
+	@Override
+	public String join(MemberVO memberVO) {
+//		memberVO.setMpassword(encoder.encode(memberVO.getMpassword()));
+		memberVO.setMpassword(password(memberVO.getMpassword()));
+		memberMapper.join(memberVO);
+		return memberVO.getMid();
+	}
+
+	/**
+     * 메서드 설명 : 회원 전화번호, 이메일, 주소 변경
+     *
+     * @param   memberVO
+     * @return  int -> 1이면 성공
+     */
+	@Override
+	public int changeInfo(MemberVO memberVO) {
+		memberVO.setMpassword(password(memberVO.getMpassword()));
+		int result = memberMapper.changeInfo(memberVO);
+		if(result > 0) return 1;
+		return 0;
+	}
+	
+	/**
+     * 메서드 설명 : 회원 이름과 전화번호로 아이디 조회
+     *
+     * @param   이름, 전화번호
+     * @return  int -> 1이면 성공
+     */
+	@Override
+	public int findID(String mname, String mtel) {
+		String result = memberMapper.findMyId(mname, mtel);
+		if(result != null) return 1;
+		return 0;
+	}
+	
 	
 }
