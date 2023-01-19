6e235cd49137496885b24f1eb80f9afef0ffd3cb
Shingiwon <65998782+Shingiwon@users.noreply.github.com>
2023-01-19 09:22:41 +0900

    edit home jsp Live Logic #70

--- LiveCommerce/src/main/java/com/livecommerce/project/HomeController.java
+++ LiveCommerce/src/main/java/com/livecommerce/project/HomeController.java
@@ -6,15 +6,22 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.livecommerce.project.service.LiveService;
+
+import lombok.extern.log4j.Log4j;
+
 /**
  * Handles requests for the application home page.
  */
 @Controller
+@Log4j
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
@@ -22,6 +29,12 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
+	@Autowired
+	private LiveService liveService;
+	
+	@Value("${video.url}")
+	private String url;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
@@ -32,7 +45,9 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		
+		model.addAttribute("url", url);
+		model.addAttribute("live", liveService.liveList());
+		log.info(url);
 		return "home";
 	}
 	
